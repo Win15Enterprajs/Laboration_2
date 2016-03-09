f7d3d125a75c040bfdde5ea8159c458974d09d71
Roppin.L <robin.ljungqvist@gmail.com>
2016-03-09 13:43:49 +0100

    Added ChessGame solution

---
+++ ChessGame/ChessGameLogic/Pieces.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessGameLogic.Packages;
+
+namespace ChessGameLogic
+{
+    abstract class Pieces
+    {
+        public double _PieceValue { get; set; }
+        public Point _CurrentPosition { get; set; }
+        public Move _BestPossibleMove { get; set; }
+        public List<Move> ListOfPossibleMoves;
+
+        public Pieces(double PieceValue, Point CurrentPosition)
+        {
+            _PieceValue = PieceValue;
+            _CurrentPosition = CurrentPosition;
+        }
+
+        public virtual void CalculateMove()
+        {
+
+        }
+
+        private void WithinBoard()
+        {
+
+        }
+        private void Encounter()
+        {
+
+        }
+        private void ValidMoves()
+        {
+
+        }
+
+
+    }
+}
