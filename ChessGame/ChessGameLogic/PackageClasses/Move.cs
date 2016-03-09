49cf03a9c1f6a45c9a039596e61dbc2f4eddb521
Roppin.L <robin.ljungqvist@gmail.com>
2016-03-09 18:37:09 +0100

    Renamed some stuff to not interfere with .gitignore

---
+++ ChessGame/ChessGameLogic/PackageClasses/Move.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameLogic.Packages
+{
+    class Move
+    {
+        public Point endPositions { get; set; }
+        public double value { get; set; }
+
+        public Move(Point parameterPoint, double parameterValue)
+        {
+            endPositions = parameterPoint;
+            this.value = parameterValue;
+        }
+
+        public Move(int x, int y, double parameterPoint)
+        {
+            endPositions = new Point(x, y);
+            value = parameterPoint;
+        }
+
+    }
+}
---
+++ ChessGame/ChessGameLogic/PackageClasses/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameLogic.Packages
+{
+    class Point
+    {
+        public int _PosX { get; set; }
+        public int _PosY { get; set; }
+
+        public Point(int x, int y)
+        {
+            _PosX = x;
+            _PosY = y;
+        }
+    }
+}
