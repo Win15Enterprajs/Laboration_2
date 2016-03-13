855f70ce888475676ba198188705c381d0231abd
Drunkenwhisp <Kevinkarlberg@hotmail.se>
2016-03-13 19:32:22 +0100

    Added in MovementLogic class

--- ChessGame/ChessGameLogic/MoveLogic.cs
+++ ChessGame/ChessGameLogic/MoveLogic.cs
@@ -8,6 +8,41 @@ namespace ChessGameLogic
 {
     class MoveLogic
     {
+        public void PawnMovement()
+        {
 
+        }
+        public void RookMovement()
+        {
+
+        }
+        public void QueenMovement()
+        {
+
+        }
+        public void KingMovement()
+        {
+
+        }
+        public void HorseMovement()
+        {
+
+        }
+        public void BishopMovement()
+        {
+
+        }
+        private void AddDiagonalMove()
+        {
+
+        }
+        private void AddHorizontalMove()
+        {
+
+        }
+        private void AddVerticalMove()
+        {
+
+        }
     }
 }
