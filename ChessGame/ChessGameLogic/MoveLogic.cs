0bbcca2c1e6181896f78be3b2354a3b9dab7538e
Drunkenwhisp <Kevinkarlberg@hotmail.se>
2016-03-13 20:09:13 +0100

    CHanges in Movelogic

--- ChessGame/ChessGameLogic/MoveLogic.cs
+++ ChessGame/ChessGameLogic/MoveLogic.cs
@@ -8,32 +8,44 @@ namespace ChessGameLogic
 {
     class MoveLogic
     {
+        private List<Move> templist = new List<Move>();
         public void ReturnMovementList(Pieces piece)
         {
-            if (piece == Pawn)
+            if (piece is Pawn)
                 PawnMovement(piece);
+            if (piece is Rook)
+                RookMovement(piece);
+            if (piece is Queen)
+                QueenMovement(piece);
+            if (piece is King)
+                KingMovement(piece);
+            if (piece is Horse)
+                HorseMovement(piece);
+            if (piece is Bishop)
+                BishopMovement(piece);
+
         }
-        public void PawnMovement(Pawn pawn)
+        public void PawnMovement(Pieces pawn)
         {
 
         }
-        public void RookMovement()
+        public void RookMovement(Pieces rook)
         {
 
         }
-        public void QueenMovement()
+        public void QueenMovement(Pieces queen)
         {
 
         }
-        public void KingMovement()
+        public void KingMovement(Pieces king)
         {
 
         }
-        public void HorseMovement()
+        public void HorseMovement(Pieces horse)
         {
 
         }
-        public void BishopMovement()
+        public void BishopMovement(Pieces bishop)
         {
 
         }
