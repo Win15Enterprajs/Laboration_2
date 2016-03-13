268b7ffdae84a9ce7a89655d8edb68a829fba4e9
RobinLjungqvist <robin.ljungqvist@gmail.com>
2016-03-13 22:10:34 +0100

    Added constructors for the classes that inherit from Pieces.

--- ChessGame/ChessGameLogic/Horse.cs
+++ ChessGame/ChessGameLogic/Horse.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChessGameLogic.Packages;
 
 namespace ChessGameLogic
 {
-    class Horse
+    class Horse : Pieces
     {
+        public Horse(string color, Point CurrentPosition): base ( color, CurrentPosition)
+        {
+
+        }
     }
 }
