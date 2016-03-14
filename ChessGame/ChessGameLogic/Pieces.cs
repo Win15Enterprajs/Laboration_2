fa0f35a52e472e961be784b1afa7d16f0a85e170
7acc <jacc321@gmail.com>
2016-03-14 08:35:16 +0100

    Revert "Revert "derpidorp""
    This reverts commit e31f0f4d2c3222645e091481af1b138645d5de17.

--- ChessGame/ChessGameLogic/Pieces.cs
+++ ChessGame/ChessGameLogic/Pieces.cs
@@ -13,12 +13,19 @@ namespace ChessGameLogic
         public string Color { get; set; }
         public Point CurrentPosition { get; set; }
         public List<Move> ListOfMoves;
-        public bool hasBeenMoved = false;
+        public bool HasBeenMoved = false;
 
-        public Pieces(string color, Point CurrentPosition)
+        public Pieces(string color, Point currentPosition)
         {
             this.Color = color;
-            this.CurrentPosition = CurrentPosition;
+            this.CurrentPosition = currentPosition;
+        }
+
+        public Pieces(Point currentPosition, string color, bool hasBennMoved)
+        {
+            this.Color = color;
+            this.CurrentPosition = currentPosition;
+            this.HasBeenMoved = false;
         }
 
         public List<Move> ReturnMoveList()
