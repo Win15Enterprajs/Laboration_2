3951a0e7c8e2ac691dc80f4709fd270bb32648c5
Drunkenwhisp <Kevinkarlberg@hotmail.se>
2016-03-15 19:42:37 +0100

    More changes to AI class

--- ChessGame/ChessGameLogic/AI.cs
+++ ChessGame/ChessGameLogic/AI.cs
@@ -24,11 +24,34 @@ namespace ChessGameLogic
                     if (item.endPositions == opponent.CurrentPosition)
                     {
                         if (opponent.PieceType == ChessPieceSymbol.Bishop)
+                            item.value = 3;
+                        if (opponent.PieceType == ChessPieceSymbol.Horse)
+                            item.value = 3;
+                        if (opponent.PieceType == ChessPieceSymbol.Pawn)
+                            item.value = 3;
+                        if (opponent.PieceType == ChessPieceSymbol.Queen)
+                            item.value = 9;
+                        if (opponent.PieceType == ChessPieceSymbol.Rook)
                             item.value = 5;
                     }
                 }
                 
             }
+        }
+        private Move RemoveSelfFromValue(Move move, Pieces piece)
+        {
+            if (piece.PieceType == ChessPieceSymbol.Bishop)
+                move.value = move.value - (3 * 0.5);
+            if (piece.PieceType == ChessPieceSymbol.Horse)
+                move.value = move.value - (3 * 0.5);
+            if (piece.PieceType == ChessPieceSymbol.Queen)
+                move.value = move.value - (9 * 0.5);
+            if (piece.PieceType == ChessPieceSymbol.Rook)
+                move.value = move.value - (3 * 0.5);
+            if (piece.PieceType == ChessPieceSymbol.Pawn)
+                    move.value = move.value - (1 * 0.5);
+            return move;
+            
         }
     }
 }
