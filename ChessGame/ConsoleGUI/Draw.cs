faba4e6c50db9fd3209ff22e888bd8308529a519
RobinLjungqvist <robin.ljungqvist@gmail.com>
2016-03-16 16:23:05 +0100

    DRAW AND STUFFS

--- ChessGame/ConsoleGUI/Draw.cs
+++ ChessGame/ConsoleGUI/Draw.cs
@@ -14,20 +14,24 @@ namespace ChessGameLogic
             Console.CursorVisible = false;
             printBoard();
 
-            int startPosX = 8;
+            int startPosX = 0;
             int startPosY = 8;
+
+            int stepover = 3;
 
             foreach(var piece in game.GetGameBoard())
             {
-                int posXtoPrint = (startPosX - piece.CurrentPosition._PosX);
+                int posXtoPrint = (startPosX + piece.CurrentPosition._PosX);
                 int posYtoPrint = (startPosY - piece.CurrentPosition._PosY);
 
-                Console.SetCursorPosition(posXtoPrint, posYtoPrint);
-                if ((posXtoPrint % 2) != 1 && (posYtoPrint % 2) != 0 || (posXtoPrint % 2) != 0 && (posYtoPrint % 2) != 1)
+                Console.SetCursorPosition((posXtoPrint * 3 ) + 4, posYtoPrint);
+
+
+                if ((posXtoPrint % 2) != 0 && (posYtoPrint % 2) != 0 || (posXtoPrint % 2) != 1 && (posYtoPrint % 2) != 1)
                 {
                     Console.BackgroundColor = ConsoleColor.DarkGray;
                 }
-                else if ((posXtoPrint % 2) != 0 && (posYtoPrint % 2) != 0 || (posXtoPrint % 2) != 1 && (posYtoPrint % 2) != 1)
+                else if ((posXtoPrint % 2) != 1 && (posYtoPrint % 2) != 0 || (posXtoPrint % 2) != 0 && (posYtoPrint % 2) != 1)
                 {
                     Console.BackgroundColor = ConsoleColor.Gray;
                 }
@@ -40,14 +44,14 @@ namespace ChessGameLogic
                     Console.ForegroundColor = ConsoleColor.White;
                 }
                 var charToPrint = (char)piece.PieceType;
-                Console.Write(" " + charToPrint + " ");
+                Console.Write(charToPrint);
             }
 
         }
 
         private void printBoard()
         {
-            for (int i = 1; i < 25; i +=3)
+            for (int i = 3; i < 25; i +=3)
             {
                 for (int j = 1; j < 9; j++)
                 {
