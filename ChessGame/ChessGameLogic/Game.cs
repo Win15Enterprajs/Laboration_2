f544d600dcc215fc6f4f8977a893698153bc6130
Drunkenwhisp <Kevinkarlberg@hotmail.se>
2016-03-16 16:21:46 +0100

    Changed in Game

--- ChessGame/ChessGameLogic/Game.cs
+++ ChessGame/ChessGameLogic/Game.cs
@@ -9,6 +9,7 @@ namespace ChessGameLogic
 {
     public class Game
     {
+        int turncouter = 0;
         List<Pieces> GameBoard;
         Pieces BestPiece;
         Player Player1;
@@ -41,6 +42,7 @@ namespace ChessGameLogic
             Pieces PieceToMove = GetBestPiece();
             RemoveKilledPiece(PieceToMove);
             BustAMove(PieceToMove);
+            turncouter++;
 
             Console.WriteLine("OOOOOH YEEEA!!!");
 
@@ -52,13 +54,21 @@ namespace ChessGameLogic
             Move bestMove = new Move(-1, -1, -999);
             foreach (Pieces piece in GameBoard)
             {
-
-
+                if (turncouter % 2 == 0 && piece.PieceColor == Color.White)
                 foreach (var move in piece.ListOfMoves)
                 {
                     if (move.value > bestMove.value)
                         bestMove = move;
 
+                }
+                else if (turncouter % 2 == 1 && piece.PieceColor == Color.Black)
+                {
+                    foreach (var move in piece.ListOfMoves)
+                    {
+                        if (move.value > bestMove.value)
+                            bestMove = move;
+
+                    }
                 }
                 piece.BestMove = bestMove;
             }
