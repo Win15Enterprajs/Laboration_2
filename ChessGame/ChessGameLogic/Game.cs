0ccc7ea768b50144a8a6fcfc3f13aa907167852f
7acc <jacc321@gmail.com>
2016-03-17 21:23:17 +0100

    +1

--- ChessGame/ChessGameLogic/Game.cs
+++ ChessGame/ChessGameLogic/Game.cs
@@ -147,8 +147,7 @@ namespace ChessGameLogic
         }
         public void PlayAGame(List<Pieces> gameboard)
         {
-            do
-            {
+            
                 turncounter++;
                 var intelligence = new AI();
                 var Movement = new MoveLogic();
@@ -183,7 +182,7 @@ namespace ChessGameLogic
                 RemoveKilledPiece(BestPiece);
                 BustAMove(BestPiece);
                 turncounter++;
-            } while (true);
+             
         }
         private Pieces GetBestPiece(List<Pieces> gameboard)
         {
