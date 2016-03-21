01c2d6004fe2da385d56526830547f674dc00345
7acc <jacc321@gmail.com>
2016-03-21 13:50:44 +0100

    Added a Ai class and started to add some methods.. (no logic)

--- ChessGame/ChessGameLogic/Ai2.cs
+++ ChessGame/ChessGameLogic/Ai2.cs
@@ -9,13 +9,29 @@ namespace ChessGameLogic.ChessPieces
 {
     class Ai2
     {
-        private List<Move> EnemyMoves;
-        private List<Move> AllyMoves;
-        private List<Pieces> Gameboard;  
+        private List<Pieces> Enemies;
+        private List<Pieces> Allies;
+        private List<Pieces> Gameboard;
+        private List<Pieces> TempGameBoard;
+
+
+        public void InitiateAI(Pieces piece, List<Pieces> board)
+        {   
+            Gameboard = board;
+            TempGameBoard = new List<Pieces>(board);
+
+            Allies = GetAllies(piece.PieceColor);
+            Enemies = GetEnemies(piece.PieceColor);
+        }
         public void GiveValueToMoves(Pieces piece, List<Pieces> board)
         {
-            
+           InitiateAI(piece,board);
+
+
         }
+
+       
+
 
         private bool CanItakeSomething(Pieces piece, List<Pieces> board)
         {
@@ -29,27 +45,58 @@ namespace ChessGameLogic.ChessPieces
 
         private bool WillIgetThreatened(Pieces piece, List<Pieces> board)
         {
-            
+            return false;
         }
 
         private bool WillIBeProtected(Pieces piece, List<Pieces> board)
         {
-            
+            return false;
         }
 
         private bool WillIthreaten(Pieces piece, List<Pieces> board)
         {
-            
+            return false;
         }
 
         private bool AmIProtected(Pieces piece, List<Pieces> board)
         {
-            
+            return false;
         }
 
         private bool AmIThreatened(Pieces piece, List<Pieces> board)
         {
-            
-        } 
+            return false;
+        }
+
+        private List<Pieces> GetEnemies(Color color)
+        {
+            List<Pieces> listOfEnemies = new List<Pieces>();
+
+            foreach (Pieces P in Gameboard)
+            {
+                if(P.PieceColor != color)
+                    listOfEnemies.Add(P);
+            }
+            return listOfEnemies;
+        }
+
+        private List<Pieces> GetAllies(Color color)
+        {
+            List<Pieces> listOfAllies = new List<Pieces>();
+
+            foreach (Pieces P in Gameboard)
+            {
+                if (P.PieceColor != color)
+                    listOfAllies.Add(P);
+            }
+            return listOfAllies;
+        }
+
+        private void RestoreTempGameBoard()
+        {
+            TempGameBoard = new List<Pieces>(Gameboard);
+        }
+
+
     }
 }
