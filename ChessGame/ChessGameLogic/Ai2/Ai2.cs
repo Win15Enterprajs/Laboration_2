635687b77d83a43f6622157a9580118e5b195f0a
7acc <jacc321@gmail.com>
2016-03-23 16:31:02 +0100

    flyttade runt lite och komenterade lite i GiveValueToMove()

--- ChessGame/ChessGameLogic/Ai2/Ai2.cs
+++ ChessGame/ChessGameLogic/Ai2/Ai2.cs
@@ -37,60 +37,65 @@ namespace ChessGameLogic
                 }
 
 
-                WillIthreaten(piece, move);
+                if (WillIthreaten(piece, move))
+                {
+                    //nånting nångting
+                }
                 if (WillIgetThreatened(move, piece))
                 {
-
-                    if (AmIProtected(move, piece)) //ingen logic här i en.
-                    {
-                        RemoveSelfFromValue(move, piece);
-                    }
+                    //nånting nångting annat
                 }
-            }
-
-
-        }
-
-
-        //kollar bara om ett moves kan ta något
-        private bool CanItakeSomething(Move move)
-        {
-            foreach (Pieces E in Enemies)
-            {
-
-                if (CompareEnemyPositionToMyMove(E.CurrentPosition, move))
+                if (AmIProtected(move, piece)) //ingen logic här i en.
                 {
-                    return true;
+                    //nånting
                 }
+                RemoveSelfFromValue(move, piece); // funderar på hur denna metoden ska bestämma hur mycket som ska tas bort
+                                                  // tänkte lite på att det kunde finnas en variabel som bestämmer hur många procent som skulle tas bort
+            }                                     // som be ändras beronde på om (will i get threatned() protected() osv.. vi får ta en diskuterare
+
+
+        }
+    
+
+    //kollar bara om ett moves kan ta något
+    private bool CanItakeSomething(Move move)
+    {
+        foreach (Pieces E in Enemies)
+        {
+
+            if (CompareEnemyPositionToMyMove(E.CurrentPosition, move))
+            {
+                return true;
             }
-            return false;
         }
+        return false;
+    }
 
-        // ger movet värde beroende på vad den tar
-        private void GiveTakeValue(Move move)
-        {
-            double enemyValue = GetThretnedEnemyPiece(move).Value;
-            move.value += enemyValue;
-
-        }
-
-        private void RemoveSelfFromValue(Move move, Pieces piece)
-        {
-            move.value -= piece.Value;
-        }
-
-        // precis som det låter, kollar om movet kommer bli hotat av en motståndare
-
-
-
-
-
-
-
-
-
+    // ger movet värde beroende på vad den tar
+    private void GiveTakeValue(Move move)
+    {
+        double enemyValue = GetThretnedEnemyPiece(move).Value;
+        move.value += enemyValue;
 
     }
+
+    private void RemoveSelfFromValue(Move move, Pieces piece)
+    {
+        move.value -= piece.Value;
+    }
+
+    // precis som det låter, kollar om movet kommer bli hotat av en motståndare
+
+
+
+
+
+
+
+
+
+
+}
 }
 
 
