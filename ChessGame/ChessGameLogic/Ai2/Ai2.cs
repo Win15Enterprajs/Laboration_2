448d0287e2d2154d4b6f365293457cf4b8f198a4
7acc <jacc321@gmail.com>
2016-03-25 15:20:56 +0100

    derp

--- ChessGame/ChessGameLogic/Ai2/Ai2.cs
+++ ChessGame/ChessGameLogic/Ai2/Ai2.cs
@@ -52,7 +52,7 @@ namespace ChessGameLogic
                 {
                     valuedMove.value -= 10;
                 }
-                if (AmIProtected(move, piece)) //ingen logic här i en.
+                if (AmIProtected(piece) > 0) //ingen logic här i en.
                 {
                     //nånting
                 }
